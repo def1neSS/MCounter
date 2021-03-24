b032be40267d209fbe2b7e1e7fb3a6e8da5b61fd
def1neSS <petrakovskij.kir@yandex.ru>
2021-03-24 17:23:21 +0300

    Add project files.

---
+++ MCounter/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+
+namespace MCounter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static string check = "";
+        static bool flag = false;
+        string path = @"..\..\spend_data.txt";
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            //FileStream f = new FileStream(path, FileMode.Open, FileAccess.Read);
+            //DirectoryInfo dirInfo = new DirectoryInfo(path);
+        }
+
+        [Serializable]
+        public class product
+        {
+            public string Type { get; set; }
+            public string Additional { get; set; }
+            public string MoneySpend { get; set; }
+
+        }
+
+        private void add_spend_fun(object sender, RoutedEventArgs e)
+        {
+            string temp_add = "";
+            if (list_spend.Text != "" && Money.Text != "")
+            {
+
+                temp_add += list_spend.Text + " | " + Money.Text + " руб.";
+
+                if (adding_list_spend.Text == "")
+                {
+                    temp_add += " | Без указания доб.информации";
+                }
+                else
+                {
+                    temp_add += " | " + adding_list_spend.Text;
+                }
+                temp_add += " | " + check;
+
+                mainTextShow.Text += temp_add + "\n";
+
+                using (StreamWriter sw = new StreamWriter(path, true, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine(temp_add);
+                }
+            }
+            else
+            {
+                error_window ew = new error_window();
+                ew.error_text.Text += "\nНе указан товар или его стоимость";
+                ew.Owner = this;
+                ew.ShowDialog();
+                if ((list_spend.Text != "" && Money.Text == "") || (list_spend.Text == "" && Money.Text != "")) { flag = true; }
+            }
+
+            if (!flag)
+            {
+                list_spend.Text = "";
+                Money.Text = "";
+                adding_list_spend.Text = "";
+            }
+
+            flag = false;
+
+        }
+
+        private void need_spend_check(object sender, RoutedEventArgs e)
+        {
+            check = "Категория : нужный";
+        }
+        private void no_need_spend_check(object sender, RoutedEventArgs e)
+        {
+            check = "Категория : необязательный";
+        }
+    }
+}
---
+++ MCounter/error_window.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MCounter
+{
+    /// <summary>
+    /// Interaction logic for error_window.xaml
+    /// </summary>
+    public partial class error_window : Window
+    {
+        public error_window()
+        {
+            InitializeComponent();
+        }
+    }
+}
